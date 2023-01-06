fa94073e5ae8b103b392cc2dc0b3453571c98ab8
choiejae <ejae8319@gmail.com>
2023-01-06 17:39:18 +0900

    docs: 회원을 가입하고 조회하는 기능 구현

---
+++ 2_Spring_Core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    // 의존관계에 문제가 있는 코드
+    // MemberServiceImpl 가 MemberRepository(추상) 도 의존하고 MemoryMemberRepository(구체) 도 의존한다
+    // DIP 를 위반
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
